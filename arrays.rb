c9530c60ae70fc0de8cdc5d4dd3e42813c8d49d6
Nick Tassinari <ntass7800@gmail.com>
2023-02-07 06:43:34 -0700

    add puts statements

--- arrays.rb
+++ arrays.rb
@@ -6,3 +6,18 @@ gpas = [3.2, 4.0, 2.19, 2.8]
 
 bool = [true, false, false, true]
 
+puts pets[2]
+    #this prints out the string in the second index position
+    # which is the 3rd string 'hamster'
+
+puts fav_nums[3]
+#prints the integer in the 3rd index position 
+#which is 90
+
+puts gpas[-1]
+#prints the float that is in the -1 index 
+#this prints the final index which is 2.8
+
+puts bool[1]
+#prints the 1 index position which is the second
+#boolean, false
